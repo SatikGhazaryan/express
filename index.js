1609c12930a9fbe64682fb2c1a3cf46fdae7eee8
Satik Ghazryan <satik@Satiks-MacBook-Air.local>
2022-06-17 14:07:02 +0400

    add controllers

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
-import express from "Express";
+import express from "express";
 const app=express();
 const port=8000;
 app.listen(port, ()=>console.log("Server listening port `${port}`"));
-const contacts=[
-    {
-        id:1,
-        title:'this is contact 1',
-        imgUrl:'url',
-        isPublic:true
-    },
-    {
-        id:2,
-        title:'this is contact 2',
-        imgUrl:'url',
-        isPublic:false
-    }
-]
 
-function getContactsController(req,res){
-    return res.send({
-        data:contacts
-    })
-}
-function postContactsController(req,res){
-    contacts.push({
-        id:4,
-        title:'this is contact 4',
-        isPublic:false
-    })
-    return res.send({
-        name:contacts 
-    })
-}
+
+const {getContactsController,
+    postContactsController}= require ('./controllers/control.js')
+
 app.get('/contacts', getContactsController)
 app.post('/contacts', postContactsController)
 // app.delete()
